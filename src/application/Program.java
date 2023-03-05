523b8d3479bf1354ae339bab460565de94cf3c89
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-03-05 12:12:14 -0300

    Criado variaveis

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,24 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main (String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double l1A, l2A, l3A, l1B, l2B, l3B;
+
+        System.out.println("Enter the measures of triangle A: ");
+        l1A = sc.nextDouble();
+        l2A = sc.nextDouble();
+        l3A = sc.nextDouble();
+
+        System.out.println("Enter the measures of triangle B: ");
+        l1B = sc.nextDouble();
+        l2B = sc.nextDouble();
+        l3B = sc.nextDouble();
 
     }
 }
