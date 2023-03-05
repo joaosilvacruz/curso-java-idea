562f30d488287d4cccc72c0b1dc2994d578e31f7
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-03-05 20:50:19 -0300

    Adicionado métodos

--- src/application/ProgramWithClass.java
+++ src/application/ProgramWithClass.java
@@ -24,11 +24,8 @@ public class ProgramWithClass {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double p1 = (x.a + x.b + x.c) / 2.0;
-        double area1 = Math.sqrt(p1 * (p1 - x.a) * (p1 - x.b) * (p1 - x.c));
-
-        double p2 = (y.a + y.b + y.c) / 2.0;
-        double area2 = Math.sqrt(p2 * (p2 - y.a) * (p2 - y.b) * (p2 - y.c));
+        double area1 = x.calculateArea();
+        double area2 = y.calculateArea();
 
         System.out.printf("The area of triangle 1 is: %f %nThe area of triangle 2 is: %f %n", area1, area2);
 
