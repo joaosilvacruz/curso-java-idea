631648eda658b584dc19aa003eb09f47e356b1ad
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-03-06 09:00:54 -0300

    Adicionado métodos

--- src/applicationProduct/application/Program.java
+++ src/applicationProduct/application/Program.java
@@ -19,9 +19,25 @@ public class Program {
         System.out.print("Quantity: ");
         p1.quantity = sc.nextInt();
 
-        System.out.printf("Product data: %s, $%.2f, %d units, $%.2f %n",p1.name, p1.price, p1.quantity, p1.totalValueInStock());
-        System.out.print("Enter the number of products to be added in stock: ");
+        System.out.println("Print with toString");
+        System.out.println(p1);
+        System.out.println();
 
+        System.out.println("Print without toString");
+        System.out.printf("Product data: %s, $%.2f, %d units, $%.2f %n",p1.name, p1.price, p1.quantity, p1.totalValueInStock());
+        System.out.println();
+
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        p1.addProducts(quantity);
+
+        System.out.println("Update data: " + p1);
+
+        System.out.print("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        p1.removeProducts(quantity);
+
+        System.out.println("Update data: " + p1);
 
     }
 }
--- src/applicationProduct/entities/Product.java
+++ src/applicationProduct/entities/Product.java
@@ -11,8 +11,18 @@ public class Product {
     public void addProducts(int quantity){
         this.quantity += quantity;
     }
-    public void removerProducts(int quantity){
+    public void removeProducts(int quantity){
         this.quantity = this.quantity < quantity? this.quantity: this.quantity - quantity;
+    }
+
+    public String toString() {
+        return name
+                + ", $"
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $"
+                + String.format("%.2f", totalValueInStock());
     }
 
 }
