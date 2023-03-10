f263b57b334824cbcaa544278f472153f15c8191
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-03-10 08:31:24 -0300

    adicionado Anotacoes

---
+++ src/anotacoes/enumeracao/Program.java
@@ -0,0 +1,5 @@
+package anotacoes.enumeracao;
+
+public class Program {
+
+}
---
+++ src/anotacoes/enumeracao/entitiesEnums/OrderStatus.java
@@ -0,0 +1,9 @@
+package anotacoes.enumeracao.entitiesEnums;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+
+}
