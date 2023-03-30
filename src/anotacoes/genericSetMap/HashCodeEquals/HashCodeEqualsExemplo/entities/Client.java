1fea7aae1d0bedb0bb0ed789b0c92ad587a77de5
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-03-29 21:15:44 -0300

    Adicionado anotacoes: HashCodeEquals

---
+++ src/anotacoes/genericSetMap/HashCodeEquals/HashCodeEqualsExemplo/entities/Client.java
@@ -0,0 +1,47 @@
+package anotacoes.genericSetMap.HashCodeEquals.HashCodeEqualsExemplo.entities;
+
+import java.util.Objects;
+
+public class Client {
+    private String name;
+    private String email;
+
+    public Client(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Client client = (Client) o;
+
+        if (!Objects.equals(name, client.name)) return false;
+        return Objects.equals(email, client.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
