51ee6caaa7ff5102294b2f646ffd74d2581c7041
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-04-02 21:59:21 -0300

    Adicionado anotacoes: Lambda

--- src/anotacoes/progFuncionalLambda/interfacesFuncionais/criandoAPropriaFuncao/application/Program.java
+++ src/anotacoes/progFuncionalLambda/interfacesFuncionais/criandoAPropriaFuncao/application/Program.java
@@ -1,12 +1,15 @@
 package anotacoes.progFuncionalLambda.interfacesFuncionais.criandoAPropriaFuncao.application;
 
-import anotacoes.progFuncionalLambda.interfacesFuncionais.function.entities.Product;
+import anotacoes.progFuncionalLambda.interfacesFuncionais.criandoAPropriaFuncao.model.services.ProductService;
+import anotacoes.progFuncionalLambda.interfacesFuncionais.criandoAPropriaFuncao.entities.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         List<Product> list = new ArrayList<>();
 
         list.add(new Product("Tv", 900.00));
@@ -14,5 +17,10 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
+        ProductService ps = new ProductService();
+
+        double sum = ps.filteredSum(list);
+
+        System.out.println("Sum = " + String.format("%.2f", sum));
     }
 }
